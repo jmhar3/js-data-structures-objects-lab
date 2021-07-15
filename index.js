6db30abf1b2b185f3cc19218aa19ee5789b18526
jmhar <jmhar@protonmail.com>
2021-07-15 14:40:32 +1000

    update driver info

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const driver = {
+    name : 'name'
+}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const updatedDriver = {...driver};
+    updatedDriver[key] = value;
+    return updatedDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+    const deleteFromDriver = {...driver};
+    delete deleteFromDriver[key];
+    return deleteFromDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
